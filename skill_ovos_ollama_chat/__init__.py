1d1453fc5ec49190e5362ec073a9dfcc7aa3eea6
Bràné <branep@gmail.com>
2024-10-30 12:20:55 -0400

    test: more debug

--- skill_ovos_ollama_chat/__init__.py
+++ skill_ovos_ollama_chat/__init__.py
@@ -137,7 +137,7 @@ class OllamaChatSkill(FallbackSkill):
             self.reset_chat()
 
         self.update_chat_history("user", message.data["utterance"])
-
+        self.log.debug(f"Chat History: {self.chat_history}")
         try:
             for chunk in self.chat():
                 self.log.debug(f"Streaming from {self.model}: {look_ahead}")
