c79a1c8c0e73731fc8cd5d3e4441a360cfa05820
Bràné <branep@gmail.com>
2024-12-10 13:48:39 -0500

    just phrase to gui

--- skill_ovos_ollama_chat/__init__.py
+++ skill_ovos_ollama_chat/__init__.py
@@ -159,13 +159,7 @@ class OllamaChatSkill(FallbackSkill):
                         token_count = token_count + 1
                         phrase = phrase + token
                         if token_count > 20 or chunk["done"] or sentence_end:
-                            self.gui.show_text(
-                                self,
-                                phrase,
-                                title="Chat",
-                                override_idle=None,
-                                override_animations=False,
-                            )
+                            self.gui.show_text(phrase)
                             self.log.debug(f"Speaking: {phrase}")
                             self.speak_dialog(
                                 phrase,
