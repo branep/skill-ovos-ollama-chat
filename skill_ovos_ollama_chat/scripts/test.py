58bcab9253d776c57b6ee9dacfb6c97c277da623
Bràné <branep@gmail.com>
2024-12-10 10:08:56 -0500

    test script

--- skill_ovos_ollama_chat/scripts/test.py
+++ skill_ovos_ollama_chat/scripts/test.py
@@ -8,33 +8,34 @@ def chat():
     client = Client(host="http://ollama.lan")
     return client.chat(
         model="phi3",
-        system="Your name is Jarvis and you are located in Bolton, Ontario. Your timezone is GMT-4. You always output numbers as words instead of numerals. Your answers really succinct",
         messages=[
             {
                 "role": "system",
-                "message": "Your name is Jarvis and you are located in Bolton, Ontario. Your timezone is GMT-4. You always output numbers as words instead of numerals. Your answers really succinct",
+                "message": "Your persona is Jarvis, a helpful small language model. You are located in Bolton, Ontario. Your timezone is GMT-4. You always output numbers as words instead of numerals. Your answers really succinct. Keep your answers under 100 words, unless providing instructions.",
             },
             {
                 "role": "user",
-                "content": "What's your name?",
+                "content": "Tell me about you",
             },
         ],
         keep_alive=-1,
         stream=True,
-        options = {
-            "num_predict": 90,
-        }
+        options={
+            "num_predict": 200,
+        },
     )
 
 
 for chunk in chat():
     if look_ahead != "":
         token = chunk["message"]["content"]
-        if "." in token and bool(re.search("[0-9]", look_ahead)):
-            print(f"Found numbered list: {chunk}")
+        # if "." in token and bool(re.search("[0-9]", look_ahead)):
+        #    print(f"Found numbered list: {chunk}")
         # print(f"\"{chunk['message']['content']}\"", end="")
         print(chunk["message"]["content"], end="")
         # print(f'"{chunk}"')
         if chunk["done"]:
-            print("End")
+            print("\nEnd")
+    else:
+        print(chunk["message"]["content"], end="")
     look_ahead = chunk["message"]["content"]
